f5c470e198cd832e69282501df250facff2384c8
GroupePdP <groupe.pdp@hotmail.fr>
2013-03-03 16:57:35 +0100

    Update DBConnection.java

--- DB/DBConnection.java
+++ DB/DBConnection.java
@@ -1,68 +1,57 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.util.*;
-import java.io.*;
+/**
+ * Je modifie le fichier pour ne laisser que les éléments important. 
+**/
 
-import java.awt.event.KeyEvent;
-import javax.swing.KeyStroke;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;  
 
+   
+// Elements pour illustrer.
 
-public class DBConnection{
-    
-	private Connection conn;
-	private String user;
-	private char[] passw;	
+private Connection conn;
+private String user;
+private char[] passw;	
 
-    public DBConnection(JDialog dialog){
-	}
 
-	public Connection getConnection(){
-		return conn;
-	}
+/**
+*   Passage de MDP dans un String pour la connexion à la DB. 
+* 	Il est possible d'utiliser un élément Password qui cache le mot de passe taper. Ce dernier peut être 
+* 	ensuite passé en "char" au moyen d'une méthode mais il est nécessaire de l'avoir en String pour la
+* 	connexion.	
+**/
 
-	class ActionConnexion extends AbstractAction {
-        
-		public ActionConnexion() {
-			super("Connexion");
-        }
+String mdp="";
+for(int i=0; i<passw.length;i++) mdp+=passw[i];
 
-		public void actionPerformed(ActionEvent event) {
-            user = userT.getText();
-			passw = passwT.getPassword();
-
-            messageL.setText("  Connexion en cours... ");
-            
-            /**
-            *	Passage de MDP dans un String pour la connexion à la DB. 
-            **/
-
-			String mdp="";
-            for(int i=0; i<passw.length;i++) mdp+=passw[i];
-
-			try{	
-				/** 
-				* 	Driver et principe de connexion à la base de données.
-				**/
-				
-				Class.forName("org.postgresql.Driver");
-				String url = "jdbc:postgresql://localhost:PORT/Nom_DB";								    
-				conn = DriverManager.getConnection(url, mdp, user);
-				Statement state = conn.createStatement();
-				
- 				dispose();
-			}
-			catch(Exception e){
-				e.printStackTrace();
-				messageL.setForeground(Color.RED);
-				messageL.setText("  Erreur d'identifiant/mot de passe. ");
-				passwT.setText("");
-			}
-        }
-    }
-    
+			
+/** 
+*   Driver et principe de connexion à la base de données.
+* 	Obligatoirement dans un bloc try/catch
+**/
+try{				
+	Class.forName("org.postgresql.Driver");
+	String url = "jdbc:postgresql://localhost:PORT/Nom_DB";								    
+	conn = DriverManager.getConnection(url, mdp, user);
+	Statement state = conn.createStatement();
 }
+catch(Exception e){
+	
+	e.printStackTrace();
+	
+	//On peut penser à rénitialiser le mot de passe en cas c'échec. Ici le JTextField utilisé pour le rentrer est
+	//passwT	
+	messageL.setForeground(Color.RED);
+	messageL.setText("  Erreur d'identifiant/mot de passe. ");
+	passwT.setText("");
+}
+        
+
+/**
+* Méthode Get à prévoir selon comment on utilisera l'objet.
+**/
+public Connection getConnection(){
+	return conn;
+}
+	
