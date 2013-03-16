0fc685d5b104b8efc67e1838f8ab27102b11f74f
Adrien <awintringer@gmail.com>
2013-03-16 16:09:04 +0100

    Package core once again

--- GAT/src/core/Core.java
+++ GAT/src/core/Core.java
@@ -1,5 +1,44 @@
 package core;
 
-public class Core {
+import database_connection.DBConnection;
+import ihm.MainFrame;
 
+public class Core {
+	
+	private Projet projet;
+	private DBConnection db_connection;
+//	private Serialisation serial;
+	
+	public static void main(String[] args){
+        MainFrame mf = new MainFrame();
+	}
+
+	
+/*	public Serialisation getSerial() {
+		return serial;
+	}
+
+
+	public void setSerial(Serialisation serial) {
+		this.serial = serial;
+	}
+*/
+
+	public Projet getProjet() {
+		return projet;
+	}
+
+	public void setProjet(Projet projet) {
+		this.projet = projet;
+	}
+
+	public DBConnection getDb_connection() {
+		return db_connection;
+	}
+
+	public void setDb_connection(DBConnection db_connection) {
+		this.db_connection = db_connection;
+	}
+	
+	
 }
