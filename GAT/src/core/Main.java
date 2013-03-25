c60cde6e821ad70356f539544f665a9776320de6
Adrien <awintringer@gmail.com>
2013-03-25 02:32:40 +0100

    Changement de 3 \ en /
    Seriously Linux?

--- GAT/src/core/Main.java
+++ GAT/src/core/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args){
 
-		String emplacement = System.getProperty("user.dir").substring(0, System.getProperty("user.dir").length() -4) + "\\Projets";
+		String emplacement = System.getProperty("user.dir").substring(0, System.getProperty("user.dir").length() -4) + "/Projets";
 		Core core = new Core(emplacement);
 
 		System.out.println(PasswordManager.encrypt("testpassword123lolilol"));
