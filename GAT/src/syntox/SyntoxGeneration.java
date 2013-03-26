dd64673f9f1fa1481df3e9bab2bfe3d61ade4532
Benoît Barthés <benoit.barthes@etu.u-bordeaux1.fr>
2013-03-26 16:32:27 +0100

    implémentation propre

--- GAT/src/syntox/SyntoxGeneration.java
+++ GAT/src/syntox/SyntoxGeneration.java
@@ -1,68 +1,53 @@
 package syntox;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
-import linguistic.conceptsGestion.Concept;
-import linguistic.graphConceptsGestion.*;
+import linguistic.graphConceptsGestion.GraphConcepts;
+import linguistic.graphConceptsGestion.GraphNode;
 
 
 /**	
   * ATTENTION : EN COURS.
 **/
 
-public class SyntoxGeneration {
+public class SyntoxGeneration
+{
 
-
-	private String requestSyntox;
-	private String contained;
-	private int numberNodes;
 	
-
-	public SyntoxGeneration(GraphConcepts gc){
-		this.requestSyntox = "Axiom[PRED:"+gc.getRoot().getConcept().getName()+contained+"]";
-		this.numberNodes = gc.getNumberOfNodes();
+	public SyntoxGeneration()
+	{
 	}
 
-	/**
-	 * SyntoxGeneration syntoxRequet = new SyntoxGeneration();
-	 * Lancer "syntoxRequet.generationSyntox(gc.getRoot());"
-	 */
-	public String generationSyntox(GraphNode gn) 
+	
+	private String ChildrenRecursive (GraphNode gn)
 	{
 		int nbArgs = 0;
-		for (GraphNode g :gn.getListNodes()){
-			contained.concat("arg"+nbArgs+"[PRED:"+g.getConcept().getName()+"]");
+		String requestSyntox = "[PRED="+gn.getConcept().getName();
+		
+		for (GraphNode g :gn.getChildrenList())
+		{
+			
+			requestSyntox.concat(",arg"+nbArgs+"=");
+			
+			requestSyntox.concat(ChildrenRecursive(g));
+			
 			nbArgs++;
-			generationSyntox(g);
-			contained.concat("]");
-		}		
+
+		}
+		
+		requestSyntox.concat("]");
 		return requestSyntox;
 	}
 	
-	public String toSyntox (Concept concept){
-		return concept.generateSyntox();
-	}
+	public String generationSyntox(GraphConcepts gc) 
+	{
+		GraphNode root = gc.getRoot();
+		
+		String requestSyntox = "Axiom";
+		
+		requestSyntox.concat(ChildrenRecursive(root)); 
 
-	
-	/*
-	public void generationSyntox (GraphConcepts graphe){
-		try{
-			
-			FileWriter fw = new FileWriter (System.getProperty("user.dir").substring(0, System.getProperty("user.dir").length() -4) + File.separator + "syntox.txt");
-			
-			for (GraphNode g :graphe.getListNodes()){
-				Concept c = g.getConcept();
-				fw.write(toSyntox(c));
-			}
-			
-			fw.close();
-		}
-		catch(IOException ex) { 
-			ex.printStackTrace(); 
-		}
-	}*/
+		
+		return requestSyntox;
+	}
 
 }
 
