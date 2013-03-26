24bc8efa72600a35940518f5dad2ddbe38a68d29
GroupePdP <groupe.pdp@hotmail.fr>
2013-03-26 13:15:49 +0100

    Update SyntoxGeneration.java

--- GAT/src/syntox/SyntoxGeneration.java
+++ GAT/src/syntox/SyntoxGeneration.java
@@ -8,13 +8,45 @@ import linguistic.conceptsGestion.Concept;
 import linguistic.graphConceptsGestion.*;
 
 
+/**	
+  * ATTENTION : EN COURS.
+**/
+
 public class SyntoxGeneration {
 
+
+	private String requestSyntox;
+	private String contained;
+	private int numberNodes;
+	
+
+	public SyntoxGeneration(GraphConcepts gc){
+		this.requestSyntox = "Axiom[PRED:"+gc.getRoot().getConcept().getName()+contained+"]";
+		this.numberNodes = gc.getNumberOfNodes();
+	}
+
+	/**
+	 * SyntoxGeneration syntoxRequet = new SyntoxGeneration();
+	 * Lancer "syntoxRequet.generationSyntox(gc.getRoot());"
+	 */
+	public String generationSyntox(GraphNode gn) 
+	{
+		int nbArgs = 0;
+		for (GraphNode g :gn.getListNodes()){
+			contained.concat("arg"+nbArgs+"[PRED:"+g.getConcept().getName()+"]");
+			nbArgs++;
+			generationSyntox(g);
+			contained.concat("]");
+		}		
+		return requestSyntox;
+	}
 	
 	public String toSyntox (Concept concept){
 		return concept.generateSyntox();
 	}
 
+	
+	/*
 	public void generationSyntox (GraphConcepts graphe){
 		try{
 			
@@ -30,6 +62,7 @@ public class SyntoxGeneration {
 		catch(IOException ex) { 
 			ex.printStackTrace(); 
 		}
-	}
+	}*/
 
 }
+
