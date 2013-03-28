7666129109933d25d663159fad31ae1203038c19
Benoît Barthés <benoit.barthes@etu.u-bordeaux1.fr>
2013-03-28 16:54:26 +0100

    fk fonctionnelle

--- DB/DB_RWC_creation.sql
+++ DB/DB_RWC_creation.sql
@@ -6,7 +6,7 @@ CREATE TABLE nation (
   nat_nom VARCHAR(20) NOT NULL,
   nat_abr VARCHAR(3) NOT NULL,
   nat_poule CHAR(1) NOT NULL CHECK (nat_poule IN ('A','B','C','D'))
-) ENGINE=InnoDB;;
+) ENGINE=InnoDB;
 
 
 
@@ -14,20 +14,23 @@ CREATE TABLE stade (
        sta_id INTEGER PRIMARY KEY AUTO_INCREMENT,
        sta_nom VARCHAR(40),
        sta_ville VARCHAR(20),
-       sta_cap INTEGER) ENGINE=InnoDB;;
+       sta_cap INTEGER) ENGINE=InnoDB;
 
 CREATE TABLE matchs (
        mat_id INTEGER PRIMARY KEY AUTO_INCREMENT,
        mat_date DATE NOT NULL,
-       mat_stade INTEGER REFERENCES stade(sta_id)
-) ENGINE=InnoDB;;
+       mat_stade INTEGER,
+       FOREIGN KEY (mat_stade) REFERENCES stade(sta_id)
+) ENGINE=InnoDB;
 
 CREATE TABLE joue (
-       mat_id INTEGER REFERENCES matchs(mat_id),
-       nat_id INTEGER REFERENCES nation(nat_id),
+       mat_id INTEGER ,
+       nat_id INTEGER ,
        nat_essai INTEGER,
        nat_trans INTEGER,
        nat_penal INTEGER,
        nat_drop INTEGER,
-       PRIMARY KEY (mat_id, nat_id)
-) ENGINE=InnoDB;;
+       PRIMARY KEY (mat_id, nat_id),
+       FOREIGN KEY (mat_id) REFERENCES matchs(mat_id),
+       FOREIGN KEY (nat_id) REFERENCES nation(nat_id)
+) ENGINE=InnoDB;
