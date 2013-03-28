c391f5670c5d0c77f7721ce912cb4f8b39030286
Benoît Barthés <benoit.barthes@etu.u-bordeaux1.fr>
2013-03-28 16:42:07 +0100

    force innoDB

--- DB/DB_RWC_creation.sql
+++ DB/DB_RWC_creation.sql
@@ -6,7 +6,7 @@ CREATE TABLE nation (
   nat_nom VARCHAR(20) NOT NULL,
   nat_abr VARCHAR(3) NOT NULL,
   nat_poule CHAR(1) NOT NULL CHECK (nat_poule IN ('A','B','C','D'))
-);
+) ENGINE=InnoDB;;
 
 
 
@@ -14,13 +14,13 @@ CREATE TABLE stade (
        sta_id INTEGER PRIMARY KEY AUTO_INCREMENT,
        sta_nom VARCHAR(40),
        sta_ville VARCHAR(20),
-       sta_cap INTEGER);
+       sta_cap INTEGER) ENGINE=InnoDB;;
 
 CREATE TABLE matchs (
        mat_id INTEGER PRIMARY KEY AUTO_INCREMENT,
        mat_date DATE NOT NULL,
        mat_stade INTEGER REFERENCES stade(sta_id)
-);
+) ENGINE=InnoDB;;
 
 CREATE TABLE joue (
        mat_id INTEGER REFERENCES matchs(mat_id),
@@ -30,4 +30,4 @@ CREATE TABLE joue (
        nat_penal INTEGER,
        nat_drop INTEGER,
        PRIMARY KEY (mat_id, nat_id)
-);
+) ENGINE=InnoDB;;
