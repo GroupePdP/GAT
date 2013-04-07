4f7f54b3351c41b596b4ac8b6fd4dba53521f3b4
benoît barthés <benoit.barthes@etu.u-bordeaux1.fr>
2013-04-07 13:37:50 +0200

    supression ancien non de fichier

--- GAT/src/databaseInspection/CookingRequest.java
+++
@@ -1,9 +0,0 @@
-package databaseInspection;
-
-
-public interface CookingRequest {
-	
-	public String getRequest();
-	public void addColumn(String table , String column);
-	public void newRequest();
-}
