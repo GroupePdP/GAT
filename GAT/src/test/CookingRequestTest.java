6a293f4fb901927786764d7da4b04e830c09e0d1
Benoît Barthés <benoit.barthes@etu.u-bordeaux1.fr>
2013-04-07 03:32:23 +0200

    mise à jour du teste

--- GAT/src/test/CookingRequestTest.java
+++ GAT/src/test/CookingRequestTest.java
@@ -1,5 +1,12 @@
 package test;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import databaseConnection.DBConnectionImpl;
+import databaseInspection.Base;
+import databaseInspection.BaseFactory;
+import databaseInspection.BaseFactoryImpl;
 import databaseInspection.CookingRequest;
 import databaseInspection.CookingRequestImpl;
 
@@ -8,28 +15,60 @@ public class CookingRequestTest {
 	
 	public static void main (String[] args){
 	
+		String url ="";//jdbc:mysql://dbserver/";
+		String user = "";
+		String pwd= "";
+		DBConnectionImpl dbc = null;
+		Scanner sc = new Scanner(System.in);
+		BaseFactory bF = BaseFactoryImpl.getInstance();
+		Base myBase = null;
 		
-		CookingRequest cr = new CookingRequestImpl(null);
+		System.out.println("entrer url");
+		url = sc.nextLine();
 		
-		cr.addColumn("joueur", "nom" );
-		cr.addColumn("joueur", "prenom");
-		cr.addColumn("coincoin" , "wtf");
-		
-		String sql = cr.getRequest();
-		
-		System.out.println(sql);
+		System.out.println("entrer user");
+		user = sc.nextLine();
+
+		System.out.println("entrer pwd");
+		pwd = sc.nextLine();
 		
 		
-		sql = cr.getRequest();
-		System.out.println(sql);
 		
-		cr.newRequest();
+		dbc = new DBConnectionImpl(url,user, pwd);
+		try {
+			dbc.connection();
+			myBase = bF.extractBase(dbc);
+			dbc.disconnection();
+			
+			CookingRequest cr = new CookingRequestImpl(myBase);
+			
+			cr.addColumn("joue", "nat_essai" );
+			cr.addColumn("joue", "nat_trans");
+			cr.addColumn("nation" , "nat_nom");
+			
+			String sql = cr.getRequest();
+			
+			System.out.println(sql);
+			
+			
+			sql = cr.getRequest();
+			System.out.println(sql);
+			
+			cr.newRequest();
+			
+//			cr.addColumn("nation", "nat_nom" );
+//			cr.addColumn("nation", "nat_abr");
+//			
+//			sql = cr.getRequest();
+//			System.out.println(sql);
+			
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
-		cr.addColumn("nation", "nat_nom" );
-		cr.addColumn("nation", "nat_abr");
+		sc.close();
 		
-		sql = cr.getRequest();
-		System.out.println(sql);
 		
 	}
 
